618392403acb851b43d76c3589a3d831979e05e3
Leia Spagnola <leiaspagnola@icloud.com>
2025-09-23 14:53:18 -0400

    fix bug with muting and allow for workspace-specific threshold

--- src/extension.ts
+++ src/extension.ts
@@ -32,11 +32,7 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	// mutes autocheck notifications for 5 minutes
 	const setRemindersMuted = () => {
-		context.workspaceState.update("areRemindersMuted", true);
-
-		setTimeout(() => {
-			context.workspaceState.update("areRemindersMuted", false);
-		}, 5 * 60 * 1000);
+		context.workspaceState.update("lastMuted", Date.now());
 
 		vscode.window.showInformationMessage("You will not receive commit reminders in this workspace for 5 minutes.");
 	};
@@ -45,16 +41,20 @@ export async function activate(context: vscode.ExtensionContext) {
 
 	// checks on file save if number of +/- changes is above threshold for prompting 
 	const autoCheckChanges = vscode.commands.registerCommand('commit-reminder.autoCheckChanges', () => {
-			vscode.workspace.onDidSaveTextDocument(async () => {
-				if (!context.workspaceState.get<boolean>("areRemindersMuted")) {
-					const threshold = context.globalState.get<number>("threshold") ?? 50;
 
+			vscode.workspace.onDidSaveTextDocument(async () => {
+
+				const lastMuted = context.workspaceState.get<number>("lastMuted");
+				if (!lastMuted || lastMuted < Date.now() - 5 * 60 * 1000) {
+
+					const threshold = context.workspaceState.get<number>("threshold") ?? (context.globalState.get<number>("threshold") ?? 100);
+					
 					const diff = await repo.diff(false);
 					const [totalAdds, totalDels] = await getAddsAndDels(diff);
 
 					if (totalAdds + totalDels > threshold) {
 						const selection = await vscode.window.showWarningMessage(
-							`You have more than ${threshold} uncommitted changes. You may want to stage and commit changes now.`,
+							`You have more than ${threshold} uncommitted changes: +${totalAdds} | -${totalDels}. You may want to stage and commit changes now.`,
 							"Mute for 5 minutes"
 						);
 
@@ -66,12 +66,12 @@ export async function activate(context: vscode.ExtensionContext) {
 		});
 	});
 
-	// updates threshold with user input
-	const changeThreshold = vscode.commands.registerCommand('commit-reminder.changeThreshold', async () => {
+	// updates global threshold with user input
+	const changeGlobalDefaultThreshold = vscode.commands.registerCommand('commit-reminder.changeGlobalDefaultThreshold', async () => {
 		const input = await vscode.window.showInputBox(
 			{ 
-				prompt: "Number of uncommitted changes after which you would like to be prompted",
-				placeHolder: "50"
+				prompt: "Number of uncommitted changes after which you would like to be prompted (by default in all workspaces)",
+				placeHolder: "100"
 			});
 
 		if (!input) {
@@ -89,6 +89,29 @@ export async function activate(context: vscode.ExtensionContext) {
 		context.globalState.update("threshold", newThreshold);
 	});
 
+	// updates workspace-specific threshold with user input
+	const changeWorkspaceThreshold = vscode.commands.registerCommand('commit-reminder.changeWorkspaceThreshold', async () => {
+		const input = await vscode.window.showInputBox(
+			{ 
+				prompt: "Number of uncommitted changes after which you would like to be prompted (in this workspace only)",
+				placeHolder: "100"
+			});
+
+		if (!input) {
+			vscode.window.showErrorMessage("A value must be entered for the new threshold.");
+			return;
+		}
+
+		const newThreshold = Number.parseInt(input);
+
+		if (Number.isNaN(newThreshold) || newThreshold < 0) {
+			vscode.window.showErrorMessage("The new threshold must be a valid positive integer.");
+			return;
+		}
+
+		context.workspaceState.update("threshold", newThreshold);
+	});
+
 	// shows number of +/- changes
 	const countChanges = vscode.commands.registerCommand('commit-reminder.countChanges', async () => {
 		const diff = await repo.diff(false);
@@ -98,7 +121,7 @@ export async function activate(context: vscode.ExtensionContext) {
 	});
 
 	// push all disposables
-	context.subscriptions.push(muteReminders, autoCheckChanges, changeThreshold, countChanges);
+	context.subscriptions.push(muteReminders, autoCheckChanges, changeGlobalDefaultThreshold, changeWorkspaceThreshold, countChanges);
 
 }
 
